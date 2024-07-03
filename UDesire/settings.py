d82555b64fedfee1b2f7e34a954d7b2abe2532e3
kuldeepsaini65 <coolboyk.deepsaini@gmail.com>
2024-07-03 16:46:58 +0530

    allowed host updated

--- UDesire/settings.py
+++ UDesire/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-^50%oz0)vjx(@0n!+9qe4ald44664bq7tg_x+kz6j-%cg)jndk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
